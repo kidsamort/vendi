5e8594be63ebd2282eab3f3d13967572be78d1b9
kidsamort <kidsamort@gmail.com>
2022-09-19 10:57:02 +0300

    add color helpers

---
+++ src/style/helpers.styled.ts
@@ -0,0 +1,72 @@
+export const color = {
+  defaults: {
+    white: 'white',
+    black: 'black',
+  },
+
+  theme: {
+    dark: {
+      b0: '#FAFAFA',
+      b1: '#F5F5FF',
+      b2: '#D0D0DA',
+      b3: '#A9A9B7',
+      b4: '#8A8A98',
+      b5: '#70707C',
+      b6: '#5B5B65',
+      b7: '#40404A',
+      b8: '#2C2C35',
+      b9: '#1E1E24',
+      b10: '#0F0F13',
+    },
+    light: {
+      w0: '#F2F3F7',
+      w1: '#E2E2EA',
+      w2: '#C6CBD9',
+      w3: '#9A9AAF',
+      w4: '#7E7E8F',
+      w5: '#656575',
+      w6: '#535362',
+      w7: '#2E2E3A',
+      w8: '#262631',
+      w9: '#16161E',
+      w10: '#07070C',
+    },
+  },
+  accents: {
+    blue: {
+      azure: '#0062FF',
+      persian: '#4807EA',
+      frosty: '#06D7F6',
+      aquamarine: '#3DD598',
+
+    },
+    green: {
+      sea: '#15FFAB',
+      spring: '#62F783',
+      verdepom: '#24CA49',
+    },
+    orange: {
+      mango: '#FF8743',
+      amber: '#FE7E07',
+      shiny: '#FFB443',
+      spunky: '#FF7A2F',
+    },
+    yellow: {
+      zinc: '#FFF736',
+    },
+    purple: {
+      amethyst: '#A162F7',
+      magenta: '#E80FE9',
+    },
+    red: {
+      strawberry: '#DB3031',
+      alizarin: '#FD4438',
+    },
+    gray: {
+      thistle: '#C2B7CD',
+    },
+    pink: {
+      crayola: '#FF5CAA',
+    },
+  },
+};
