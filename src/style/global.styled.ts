775b1de7b1365691b0987d6cec82380deae9cb5f
kidsamort <kidsamort@gmail.com>
2022-09-19 09:05:52 +0300

    add global styles

---
+++ src/style/global.styled.ts
@@ -0,0 +1,6 @@
+import { createGlobalStyle, css } from 'styled-components';
+import { DefaultTheme } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle<{ theme: DefaultTheme }>`
+  
+`
