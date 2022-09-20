c9bdb98e743b1439854f8b305eba0699692df5e5
kidsamort <kidsamort@gmail.com>
2022-09-20 07:35:35 +0300

    fix transition

--- src/style/global.styled.ts
+++ src/style/global.styled.ts
@@ -6,10 +6,7 @@ import { normalize } from 'styled-normalize';
 export const GlobalStyles = createGlobalStyle<{ theme: DefaultTheme }>`
   ${normalize}
   * {
-    transition: all 0.3s ease-out;
-  }
-  div{
-    transition: all 0.05s ease-out;
+    transition: background 0.3s ease-in, color 0.08s ease-in, fill 0.3s ease-in;
   }
 
   ;
