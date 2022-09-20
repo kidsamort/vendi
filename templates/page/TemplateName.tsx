89db063344f841c87c6e1ca5ad4a48a91427d065
kidsamort <kidsamort@gmail.com>
2022-09-20 12:01:55 +0300

    fix name

--- templates/page/TemplateName.tsx
+++ templates/page/TemplateName.tsx
@@ -3,7 +3,7 @@ import { TemplateNameWrapperStyled } from './TemplateName.styled';
 
 const TemplateName: FC = () => (
   <TemplateNameWrapperStyled className='TemplateName' data-testid='TemplateName'>
-    TemplateName Component
+    TemplateName Page
   </TemplateNameWrapperStyled>
 );
 
