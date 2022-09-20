fe0b663963b996ade0e8f190676fcb9d88213835
kidsamort <kidsamort@gmail.com>
2022-09-20 12:23:43 +0300

    add router project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import { Provider } from 'react-redux';
 import { setupStore } from 'redux/store';
-
 import { GlobalStyles } from 'style/global.styled';
-import App from './App';
 import Theme from './components/theme';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from 'components/router/router';
 
 
 const store = setupStore();
@@ -14,12 +13,15 @@ const store = setupStore();
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Theme>
         <GlobalStyles />
-        <App />
+        <BrowserRouter>
+          <AppRouter />
+        </BrowserRouter>
       </Theme>
     </Provider>
   </React.StrictMode>,
