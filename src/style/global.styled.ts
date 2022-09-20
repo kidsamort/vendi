f41e977668663ec3cb052c2cea76495b15fb599b
kidsamort <kidsamort@gmail.com>
2022-09-20 07:18:13 +0300

    add transition

--- src/style/global.styled.ts
+++ src/style/global.styled.ts
@@ -5,6 +5,12 @@ import { normalize } from 'styled-normalize';
 
 export const GlobalStyles = createGlobalStyle<{ theme: DefaultTheme }>`
   ${normalize}
+  * {
+    transition: all 0.3s ease-out;
+  }
+  div{
+    transition: all 0.05s ease-out;
+  }
 
   ;
   body {
